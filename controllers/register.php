f828edfa550e455d6c31dced4992c72a95a38503
Henri Bergius <henri.bergius@iki.fi>
2012-12-11 16:51:22 +0100

    Initial POST handling

--- controllers/register.php
+++ controllers/register.php
@@ -15,22 +15,54 @@ class fi_openkeidas_registration_controllers_register
         }
 
         $user = new fi_openkeidas_registration_user();
-        $this->data['form'] = midgardmvc_helper_forms_mgdschema::create($user, false);
+        $this->data['form'] = $this->generate_form($user);
+
+        midgardmvc_core::get_instance()->head->set_title('Rekisteröityminen');
+    }
+
+    public function post_form()
+    {
+        if (midgardmvc_core::get_instance()->authentication->is_user())
+        {
+            midgardmvc_core::get_instance()->head->relocate('/');
+            return;
+        }
+
+        midgardmvc_core::get_instance()->authorization->enter_sudo('fi_openkeidas_registration'); 
+
+        // Read values from POST
+        $user = new fi_openkeidas_registration_user();
+        $form = $this->generate_form($user);
+        $form->process_post(); 
+
+        // Populate user
+        midgardmvc_helper_forms_mgdschema::form_to_object($form, $user);
+
+        $this->create_account($user);
+    }
+
+    private function generate_form(fi_openkeidas_registration_user $user)
+    {
+        $form = midgardmvc_helper_forms_mgdschema::create($user, false);
 
         // Set labels
-        $this->data['form']->firstname->widget->set_label('Etunimi');
-        $this->data['form']->lastname->widget->set_label('Sukunimi');
-        $this->data['form']->memberid->widget->set_label('OAJ:n jäsennumero');
-        $this->data['form']->email->widget->set_label('Sähköposti');
-        $this->data['form']->school->widget->set_label('Koulu');
-        $this->data['form']->municipality->widget->set_label('Kunta');
+        $form->firstname->widget->set_label('Etunimi');
+        $form->lastname->widget->set_label('Sukunimi');
+        $form->memberid->widget->set_label('OAJ:n jäsennumero');
+        $form->email->widget->set_label('Sähköposti');
+        $form->school->widget->set_label('Koulu');
+        $form->municipality->widget->set_label('Kunta');
 
         // Set all fields to required
-        foreach ($this->data['form']->items as $item)
+        foreach ($form->items as $item)
         {
             $item->set_required(true);
         }
 
-        midgardmvc_core::get_instance()->head->set_title('Rekisteröityminen');
+        return $form;
+    }
+
+    private function create_account(fi_openkeidas_registration_user $user)
+    {
     }
 }
