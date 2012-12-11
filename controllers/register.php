92d6918f3291e3f62260748cfd4a5f23c538c83a
Henri Bergius <henri.bergius@iki.fi>
2012-12-11 16:30:50 +0100

    Set labels on widgets, not fields

--- controllers/register.php
+++ controllers/register.php
@@ -18,12 +18,12 @@ class fi_openkeidas_registration_controllers_register
         $this->data['form'] = midgardmvc_helper_forms_mgdschema::create($user, false);
 
         // Set labels
-        $this->data['form']->firstname->set_label('Etunimi');
-        $this->data['form']->lastname->set_label('Sukunimi');
-        $this->data['form']->memberid->set_label('OAJ:n jäsennumero');
-        $this->data['form']->email->set_label('Sähköposti');
-        $this->data['form']->school->set_label('Koulu');
-        $this->data['form']->municipality->set_label('Kunta');
+        $this->data['form']->firstname->get_widget()->set_label('Etunimi');
+        $this->data['form']->lastname->get_widget()->set_label('Sukunimi');
+        $this->data['form']->memberid->get_widget()->set_label('OAJ:n jäsennumero');
+        $this->data['form']->email->get_widget()->set_label('Sähköposti');
+        $this->data['form']->school->get_widget()->set_label('Koulu');
+        $this->data['form']->municipality->get_widget()->set_label('Kunta');
 
         // Set all fields to required
         foreach ($this->data['form']->items as $item)
