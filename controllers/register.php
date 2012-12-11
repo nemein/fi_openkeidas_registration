3e707d40ce9104daae7680d47aee13017a9ccc52
Henri Bergius <henri.bergius@iki.fi>
2012-12-11 16:32:10 +0100

    Correct usage

--- controllers/register.php
+++ controllers/register.php
@@ -18,12 +18,12 @@ class fi_openkeidas_registration_controllers_register
         $this->data['form'] = midgardmvc_helper_forms_mgdschema::create($user, false);
 
         // Set labels
-        $this->data['form']->firstname->get_widget()->set_label('Etunimi');
-        $this->data['form']->lastname->get_widget()->set_label('Sukunimi');
-        $this->data['form']->memberid->get_widget()->set_label('OAJ:n jäsennumero');
-        $this->data['form']->email->get_widget()->set_label('Sähköposti');
-        $this->data['form']->school->get_widget()->set_label('Koulu');
-        $this->data['form']->municipality->get_widget()->set_label('Kunta');
+        $this->data['form']->firstname->widget->set_label('Etunimi');
+        $this->data['form']->lastname->widget->set_label('Sukunimi');
+        $this->data['form']->memberid->widget->set_label('OAJ:n jäsennumero');
+        $this->data['form']->email->widget->set_label('Sähköposti');
+        $this->data['form']->school->widget->set_label('Koulu');
+        $this->data['form']->municipality->widget->set_label('Kunta');
 
         // Set all fields to required
         foreach ($this->data['form']->items as $item)
